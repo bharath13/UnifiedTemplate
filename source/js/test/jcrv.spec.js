5ea0ed8c2492e7f446f3b329a6032ac21223cddc
Sothish Sasidharan <sothish@gmail.com>
2016-09-25 23:19:58 -0400

    UT-9 Validate article json

--- source/js/test/jcrv.spec.js
+++ source/js/test/jcrv.spec.js
@@ -1,10 +1,18 @@
 var jcr = require('../../../vendor/jcr/jcrv.js');
 var expect = require('chai').expect;
 
-describe('jcr testing suite - Test artcile.json', function() {
-    it('should validate the reference article file', function() {
+describe('jcr testing suite', function() {
+
+    it('should validate the reference file - article.json', function() {
         expect(function(){
-            jcr('source/_patterns/04-pages/article.json', 'source/_patterns/04-pages/article.json');
-        }).to.throw(Error);
+            jcr('source/_patterns/04-pages/article.jcr', '../../source/_patterns/04-pages/article.json');
+        }).to.not.throw(Error);
     });
+
+    it('should validate the reference file - article-title.json', function() {
+        expect(function(){
+            jcr('source/_patterns/00-atoms/02-text/article-title.jcr', '../../source/_patterns/00-atoms/02-text/article-title.json');
+        }).to.not.throw(Error);
+    });
+
 });
